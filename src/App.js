3d9cc9faeb1ba8353fb3781a716e6212fc9f552e
Franco Du Plessis <67424625+Franky-Dee@users.noreply.github.com>
2023-12-11 14:07:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -51,6 +51,22 @@ function Friend({ friend }) {
         alt={friend.name}
       />
       <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} R{Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you R{Math.abs(friend.balance)}
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You owe {friend.name} are even</p>}
+
+      <button className="button">Select</button>
     </li>
   );
 }
